f2fcb751ad23318a8f9f34abc342700c18920889
Elitelord <73070716+Elitelord@users.noreply.github.com>
2024-01-27 18:01:36 -0600

    Add form field

--- KintoneAPI.js
+++ KintoneAPI.js
@@ -1,18 +1,39 @@
 var body = {
-    'apps': [
-      {
-        'app': 1,
-        'revision': 57
+    'app': 1,
+    'properties': {
+      'Text__single_line_1': {
+        'type': 'SINGLE_LINE_TEXT',
+        'code': 'Text__single_line_1',
+        'label': 'Text (single-line)',
+        'noLabel': false,
+        'required': true,
+        'unique': true,
+        'maxLength': 64,
+        'minLength': 0,
+        'defaultValue': '',
+        'expression': '',
+        'hideExpression': false
       },
-      {
-        'app': 1001,
-        'revision': 22
+      'Number': {
+        'type': 'NUMBER',
+        'code': 'Number',
+        'label': 'Number',
+        'noLabel': true,
+        'required': false,
+        'unique': false,
+        'maxValue': 64,
+        'minValue': 0,
+        'defaultValue': '12345',
+        'digit': true,
+        'displayScale': '',
+        'expression': '',
+        'unit': '$',
+        'unitPosition': 'BEFORE'
       }
-    ],
-    'revert': true
+    }
   };
   
-  kintone.api(kintone.api.url('/k/v1/preview/app/deploy.json', true), 'POST', body, function(resp) {
+  kintone.api(kintone.api.url('/k/v1/preview/app/form/fields.json', true), 'POST', body, function(resp) {
     // success
     console.log(resp);
   }, function(error) {
